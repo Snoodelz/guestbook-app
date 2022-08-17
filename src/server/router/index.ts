4b987245a448d0c5012b6a7d4b005ad313c36890
Snoodelz <ludwig.mostrom@gmail.com>
2022-08-17 17:14:33 +0200

    First commit

---
+++ src/server/router/index.ts
@@ -0,0 +1,12 @@
+// src/server/router/index.ts
+import { createRouter } from "./context";
+import { guestbookRouter } from "./guestbook";
+import superjson from "superjson";
+
+
+export const appRouter = createRouter()
+  .transformer(superjson)
+  .merge("guestbook.", guestbookRouter)
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
